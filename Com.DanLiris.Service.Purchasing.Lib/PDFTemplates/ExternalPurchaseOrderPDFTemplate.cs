526ba9a3e6b666397e4641bbcb7988f2d53af022
Kornelius Septa Jasa Sipayung <61999928+writecodex09@users.noreply.github.com>
2022-06-02 14:05:38 +0700

    Update PPN 11%

--- Com.DanLiris.Service.Purchasing.Lib/PDFTemplates/ExternalPurchaseOrderPDFTemplate.cs
+++ Com.DanLiris.Service.Purchasing.Lib/PDFTemplates/ExternalPurchaseOrderPDFTemplate.cs
@@ -190,7 +190,8 @@ namespace Com.DanLiris.Service.Purchasing.Lib.PDFTemplates
             tableContent.AddCell(cellRightMerge);
 
             cellRight.Colspan = 4;
-            cellRight.Phrase = new Phrase($"PPN {viewModel.vatRate}%", bold_font);
+            cellRight.Phrase = new Phrase("PPN 11%", bold_font);
+            //cellRight.Phrase = new Phrase($"PPN {viewModel.vatRate}%", bold_font);
             tableContent.AddCell(cellRight);
 
             cellLeftMerge.Phrase = new Phrase($"{viewModel.currency.code}", smaller_font);
@@ -200,7 +201,8 @@ namespace Com.DanLiris.Service.Purchasing.Lib.PDFTemplates
             double ppnNominal = 0;
             if (viewModel.useVat)
             {
-                ppnNominal = total * viewModel.vatRate / 100;
+                ppnNominal = total * 11 / 100;
+                //ppnNominal = total * viewModel.vatRate / 100;
                 ppn = $"{ppnNominal.ToString("N", new CultureInfo("id-ID"))}";
             }
             cellRightMerge.Phrase = new Phrase(ppn, smaller_font);
